55818ee07d3c0ba53aa253d0912fc1b8e1866ef4
Neil Danner <ndanner22@gmail.com>
2024-01-19 11:54:06 +0000

    update of package.json for pg-format

--- db/connection.js
+++ db/connection.js
@@ -11,8 +11,8 @@ if (ENV === "production") {
   config.max = 2;
 }
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
