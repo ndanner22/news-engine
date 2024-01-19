0ee8c1c1343241c856eaf234a7c08a489626ef93
Neil Danner <ndanner22@gmail.com>
2024-01-19 10:44:52 +0000

    Set up for instance of database online

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,18 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+const config = {};
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
 
 module.exports = new Pool();
